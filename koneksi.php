65adb1ba915da095bb375303fa36995244800940
riaanjeli314 <91247352+riaanjeli314@users.noreply.github.com>
2023-06-06 11:36:47 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+
+    function getConnection() {
+        return
+        new PDO("mysql:host=localhost;dbname=mahasiswa", "root", "", [PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
+    }
+?>
